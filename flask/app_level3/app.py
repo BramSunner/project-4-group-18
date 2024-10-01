c865e8095b53210f0808e6601fd3ed1fb9a13eff
Jorge Arroyo <arroyo.jorge2017@gmail.com>
2024-10-01 08:49:19 -0500

    flask

---
+++ flask/app_level3/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, redirect, request, jsonify
+from modelHelper import ModelHelper
+
+# Create an instance of Flask
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+modelHelper = ModelHelper()
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    # Return template and data
+    return render_template("index.html")
+
+@app.route("/work_cited")
+def work_cited():
+    # Return template and data
+    return render_template("work_cited.html")
+
+@app.route("/about_us")
+def about_us():
+    # Return template and data
+    return render_template("about_us.html")
+
+@app.route("/tableau")
+def tableau():
+    # Return template and data
+    return render_template("tableau.html")
+    
+@app.route("/makePredictions", methods=["POST"])
+def make_predictions():
+    content = request.json["data"]
+    print(content)
+    return(jsonify({"ok": True}))
+
+    # make this section lineup with what we have in the model helper
+
+
+#############################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+#main
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask/app_level3/modelHelper.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pickle
+import numpy as np
+
+class ModelHelper():
+    def __init__(self):
+        pass
+
+#   Insert our prediction model into this spot
